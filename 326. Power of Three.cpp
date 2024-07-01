7a521b8a55e7fb3e7c8c447df68e832d6d27845c
TimmyLU <51018765+TimmyLU@users.noreply.github.com>
2024-07-02 01:26:12 +0800

    Update 326. Power of Three.cpp

--- 326. Power of Three.cpp
+++ 326. Power of Three.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 bool isPowerOfThree(int n) {
-    return n > 0 && 1162261467 % n == 0; //1162261467 = 3^19
+    return n > 0 && 1162261467 % n == 0; //1162261467 = 3^19 ---> int範圍中最大的3^n次方
 }
 
 int main() {
