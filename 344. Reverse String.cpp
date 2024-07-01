b78ee32e357bbc3d1c93abd726c1f48185271aef
TimmyLU <51018765+TimmyLU@users.noreply.github.com>
2024-07-02 02:45:14 +0800

    Add files via upload

---
+++ 344. Reverse String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+        for(;left < right;left++, right--){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+        }
+    }
+};
